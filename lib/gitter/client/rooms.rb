1d161648f284145875dd81e909ebf5412bb04feb
kristenmills <kristen@kristen-mills.com>
2014-07-26 15:25:48 -0700

    Room get requests

--- lib/gitter/client/rooms.rb
+++ lib/gitter/client/rooms.rb
@@ -1,8 +1,30 @@
+require 'hashie/mash'
+
 module Gitter
   class Client
     module Rooms
       def rooms
-        self.class.get("/rooms", { headers: @headers })
+        room_list = []
+        self.class.get("/rooms", { headers: @headers }).each do |room|
+          room_list << Hashie::Mash.new(room)
+        end
+        room_list
+      end
+
+      def room_users(room_id)
+        user_list = []
+        self.class.get("/rooms/#{room_id}/users", { headers: @headers }).each do |user|
+          user_list << Hashie::Mash.new(user)
+        end
+        user_list
+      end
+
+      def room_channels(room_id)
+        channel_list = []
+        self.class.get("/rooms/#{room_id}/channels", { headers: @headers }).each do |channel|
+          channel_list << Hashie::Mash.new(channel)
+        end
+        channel_list
       end
     end
   end
